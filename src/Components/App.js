5b7507c5f0bc9f0061cb100de2c485e0cc8949ac
DESKTOP-TOOTMLU\DuyLongPc <LongTMGCS16297@fpt.edu.vn>
2019-01-01 09:34:36 +0700

    SelectedVideo

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
-    state = { videos: [] }
+    state = { videos: [], selectedVideo: null }
 
     onSearchSubmit = async (sData) => {
         const res = await youtube.get('/search', {
@@ -13,14 +14,18 @@ class App extends React.Component {
             }
         });
         this.setState({ videos: res.data.items });
+    }
 
+    onVideoSelect = (video) => {
+        this.setState({ selectedVideo: video });
     }
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onSearchSubmit} />
-                <VideoList videos={this.state.videos}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
             </div>
         );
     }
--- src/Components/VideoList.js
+++ src/Components/VideoList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
     const renderList = videos.map((video) => {
-        return <VideoItem key={video.id.videoId} video={video} />
+        return <VideoItem key={video.id.videoId} video={video} onVideoSelect={onVideoSelect} />
     })
     return (
-        <div>
+        <div className="ui relaxed divided list">
             {renderList}
         </div>
     );
